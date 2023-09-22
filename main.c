ffb8729cf5bca24d7c3aedca1649a681ad8dd8a9
Andrew March <youthclubrecords@gmail.com>
2023-09-22 16:56:39 -0700

    text dsp init

--- main.c
+++ main.c
@@ -2,10 +2,12 @@
 #include "board.h"
 #include "reverb.h"
 
+#define BUF_SIZE 256
+
 mydsp *dsp = (mydsp *)BFLB_PSRAM_BASE;
 
-float buf_in[256];
-float buf_out[256];
+float buf_in[BUF_SIZE];
+float buf_out[BUF_SIZE];
 float *buf_p_in[1];
 float *buf_p_out[1];
 
@@ -29,7 +31,7 @@ int main(void)
 
     while (1) {
         printf("main loop");
-        computemydsp(dsp, 256, buf_p_in, buf_p_out);
+        computemydsp(dsp, BUF_SIZE, buf_p_in, buf_p_out);
         bflb_mtimer_delay_ms(1000);
     }
 }
