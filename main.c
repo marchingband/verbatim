09e56ce740dfe861d5208811f9788f6357e21c7a
Andrew March <youthclubrecords@gmail.com>
2023-09-22 16:48:46 -0700

    text dsp init

--- main.c
+++ main.c
@@ -4,12 +4,16 @@
 
 mydsp *dsp = (mydsp *)BFLB_PSRAM_BASE;
 
+float buf_in[256];
+float buf_out[256];
+
 void dsp_init(size_t buf_size)
 {
     initmydsp(&dsp, 48000);
     // buildUserInterfacemydsp(&dsp, &ui_glue);
     // buf[0] = (float *)malloc(buf_size * sizeof(float));
 }
+
 
 int main(void)
 {
@@ -20,6 +24,7 @@ int main(void)
 
     while (1) {
         printf("main loop");
+        computemydsp(dsp, 256, buf_in, buf_out);
         bflb_mtimer_delay_ms(1000);
     }
 }
